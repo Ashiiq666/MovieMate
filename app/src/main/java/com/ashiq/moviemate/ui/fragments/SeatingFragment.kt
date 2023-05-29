42366cbb67fbc14437a5b12dc729b6e9ca12d395
Ashiiq666 <ashiiq666@gmail.com>
2023-05-29 10:38:54 +0530

    Initial commit

---
+++ app/src/main/java/com/ashiq/moviemate/ui/fragments/SeatingFragment.kt
@@ -0,0 +1,26 @@
+package com.ashiq.moviemate.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ashiq.moviemate.R
+
+
+class SeatingFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_seating, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/ashiq/moviemate/utils/Constant.kt
@@ -0,0 +1,4 @@
+package com.ashiq.moviemate.utils
+
+object Constant {
+}
