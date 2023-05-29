0da8f2ba900d9fcbe7c5592e2cd043295e8419c5
Ashiiq666 <ashiiq666@gmail.com>
2023-05-29 19:49:18 +0530

    created fragments

--- app/src/main/java/com/ashiq/moviemate/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ashiq/moviemate/ui/activity/MainActivity.kt
@@ -2,11 +2,74 @@ package com.ashiq.moviemate.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.ashiq.moviemate.R
+import com.ashiq.moviemate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private var navController: NavController? = null
+    private var navHostFragment: NavHostFragment? = null
+    private var graph: NavGraph? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        init()
+        handleEvents()
+    }
+
+    private fun init() {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val inflater = navHostFragment?.navController?.navInflater
+        graph = inflater?.inflate(R.navigation.nav_graph)
+        setDestination()
+    }
+
+    private fun setDestination() {
+
+        graph?.setStartDestination(R.id.homeFragment)
+
+
+        navController = navHostFragment?.navController
+        graph?.let { navController?.setGraph(it, intent.extras) }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.homeFragment -> {
+                navController?.navigate(R.id.homeFragment)
+                binding?.drawerLayout?.close()
+            }
+            R.id.nav_order -> {
+                navController?.navigate(R.id.orderListFragment)
+                binding?.drawerLayout?.close()
+            }
+            R.id.nav_profile -> {
+                navController?.navigate(R.id.profileFragment)
+                binding?.drawerLayout?.close()
+            }
+            R.id.nav_logout -> {
+                navController?.navigate(R.id.loginFragment)
+                binding?.drawerLayout?.close()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun handleEvents() {
+        binding?.menuDrawer?.setOnClickListener {
+            binding?.drawerLayout?.open()
+        }
+        binding?.root?.setOnClickListener {
+            binding?.drawerLayout?.close()
+        }
     }
 }
--- app/src/main/java/com/ashiq/moviemate/ui/fragments/CinemaFragment.kt
+++ app/src/main/java/com/ashiq/moviemate/ui/fragments/CinemaFragment.kt
@@ -5,26 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.ashiq.moviemate.ARG_PARAM1
-import com.ashiq.moviemate.ARG_PARAM2
 import com.ashiq.moviemate.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CinemaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class CinemaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,23 +24,4 @@ class CinemaFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_cinema, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CinemaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CinemaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
